a1f4b1e6fbe32643735c7997ca2ef8caa029a7bb
Caique <caiquegomesdev@gmail.com>
2024-01-24 20:43:07 -0300

    Typescript

---
+++ 001 - Inicio/001 - Instalacao/index.js
@@ -0,0 +1,6 @@
+// Typescript 
+// npm install -g typescript
+//tsc --init
+//pluglin: Coder Runer
+// npm i -g ts-node
+//ctrl + alt + n
---
+++ 001 - Inicio/001 - Instalacao/index.ts
@@ -0,0 +1,14 @@
+// Typescript 
+// npm install -g typescript
+//tsc --init
+
+//pluglin: Coder Runer
+// npm i -g ts-node
+//ctrl + alt + n
+
+console.log("npm install -g typescript");
+
+const pedido = (pedido: string) => {
+    return `Deu bom o seu pedido ${pedido}`
+}
+console.log(pedido("123"));
